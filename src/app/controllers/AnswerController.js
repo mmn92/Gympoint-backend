0b76c19dde9cc6019ac4a6aad4ef2800663c88a9
mmn92 <matheus.mota.nogueira@gmail.com>
2019-11-11 16:15:54 -0300

    add email sending to anser controller

--- src/app/controllers/AnswerController.js
+++ src/app/controllers/AnswerController.js
@@ -1,4 +1,9 @@
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
 import Order from '../schemas/Order';
+import Mail from '../../lib/Mail';
+import Student from '../models/Student';
 
 class AnswerController {
   async index(req, res) {
@@ -20,6 +25,22 @@ class AnswerController {
       },
       { new: true }
     );
+
+    const student = await Student.findByPk(order.student);
+
+    await Mail.sendMail({
+      to: `${student.name} <${student.email}>`,
+      subject: 'Nova resposta',
+      template: 'answered',
+      context: {
+        student: student.name,
+        question: order.question,
+        answer: order.answer,
+        date: format(order.answeredAt, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
 
     return res.json(order);
   }
